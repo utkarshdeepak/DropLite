a046bdc7de33d72c8aa37592856526da11ce0724
utkarshdeepak <udeepak@ph.iitr.ac.in>
2025-06-22 03:46:45 +0530

    Set up Spring Initializr

---
+++ src/main/java/com/utkarshdeepak/droplite/DropLiteApplication.java
@@ -0,0 +1,13 @@
+package com.utkarshdeepak.droplite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utkarshdeepak/droplite/DropLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utkarshdeepak.droplite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
