4707fb009dc5593df35b7578048f69f623bfe809
utkarshdeepak <udeepak@ph.iitr.ac.in>
2025-06-22 13:17:58 +0530

    Fixed startup script

--- db-scripts/01_create_tables.sql
+++ db-scripts/01_create_tables.sql
@@ -1,6 +1,26 @@
-DROP TABLE files;
+ALTER SESSION SET CONTAINER=FREEPDB1;
 
-CREATE TABLE files (
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE droplite.file_chunks';
+EXCEPTION
+    WHEN OTHERS THEN
+        IF SQLCODE != -942 THEN
+            RAISE;
+        END IF;
+END;
+/
+
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE droplite.files';
+EXCEPTION
+    WHEN OTHERS THEN
+        IF SQLCODE != -942 THEN
+            RAISE;
+        END IF;
+END;
+/
+
+CREATE TABLE droplite.files (
        id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
        file_name VARCHAR2(255) NOT NULL,
        file_type VARCHAR2(20),
@@ -10,7 +30,7 @@ CREATE TABLE files (
        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE file_chunks (
+CREATE TABLE droplite.file_chunks (
        id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
        file_id NUMBER NOT NULL,
        chunk_index INTEGER NOT NULL,
